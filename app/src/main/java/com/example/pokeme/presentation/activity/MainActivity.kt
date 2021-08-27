6b52ae772f70da68c4025eb4d90c60ca956da8eb
ysur67 <rus.muratow@gmail.com>
2021-08-27 18:03:17 +0300

    fix: change buttons to tabslayout

--- app/src/main/java/com/example/pokeme/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/pokeme/presentation/activity/MainActivity.kt
@@ -2,29 +2,50 @@ package com.example.pokeme.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import com.example.pokeme.R
 import com.example.pokeme.databinding.ActivityMainBinding
-import com.example.pokeme.domain.MessageViewModel
+import com.example.pokeme.domain.UserViewModel
 import com.example.pokeme.presentation.fragment.auth.LoginFragment
 import com.example.pokeme.presentation.fragment.auth.RegisterFragment
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val userViewModel: UserViewModel by viewModels()
+
+    private val tabSelectedListener = object : TabLayout.OnTabSelectedListener {
+        override fun onTabSelected(tab: TabLayout.Tab?) {
+            when (tab?.contentDescription) {
+                resources.getString(R.string.loginTabItem) -> {
+                    val loginFragment = LoginFragment.newInstance()
+                    changeFragment(loginFragment)
+                }
+                resources.getString(R.string.registerTabItem) -> {
+                    val registerFragment = RegisterFragment.newInstance()
+                    changeFragment(registerFragment)
+                }
+            }
+        }
+
+        override fun onTabUnselected(tab: TabLayout.Tab?) {
+        }
+
+        override fun onTabReselected(tab: TabLayout.Tab?) {
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-        binding.checkoutLogin.setOnClickListener{
-            val loginFragment = LoginFragment.newInstance()
-            changeFragment(loginFragment)
-        }
-        binding.checkoutRegister.setOnClickListener{
-            val registerFragment = RegisterFragment.newInstance()
-            changeFragment(registerFragment)
-        }
+        binding.tabLayout.addOnTabSelectedListener(tabSelectedListener)
+        userViewModel.isLoading.observe(this, {
+            binding.tabLayout.isEnabled = !it
+        })
     }
 
     private fun changeFragment(new: Fragment) {
